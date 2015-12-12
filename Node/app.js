1ce572e05a40aec85c619727942eb8f8dc95bd22
Sahand Nayebaziz <sahand@sahand.me>
2015-12-12 14:09:43 -0800

    add node socket server

---
+++ Node/app.js
@@ -0,0 +1,31 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+shell = require('shelljs');
+
+app.listen(1993);
+
+function handler (req, res) {
+	fs.readFile(__dirname + '/index.html',
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+
+			res.writeHead(200);
+			res.end(data);
+		});
+}
+
+io.on('connection', function (socket) {
+	console.log("+");
+	socket.on('message', function (data) {
+		var code = data.code;
+		code = "#!/usr/bin/env swift\n" + code; 
+		var fileName = "code.swift";
+		code.to(fileName);
+		shell.chmod("+x", fileName);
+		shell.exec("./" + fileName); // get data with callback?
+	});
+});
